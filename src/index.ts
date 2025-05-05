77c80db7c7492ba6426d7e7daa24d7b3ad20a0ab
martabal <74269598+martabal@users.noreply.github.com>
2025-05-05 18:15:14 +0200

    refactor: simplify usage

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import { clean } from "./clean";
+import { ff_diff } from "./diff";
+
+(async () => {
+  const firstArg = process.argv[2];
+
+  if (firstArg == "clean") {
+    clean();
+    return;
+  }
+  const version1 = firstArg;
+  const version2 = process.argv[3];
+  if (!version1 || !version2) {
+    console.error(
+      `Usage: 
+      ff_diff clean [--keep <version1>,<version2>] [--keep-archives] [--keep-sources]
+      ff_diff <version1> <version2> [--clean-archives] [--clean-sources] [--do-not-print-diffs-in-console] [--save-diffs-in-file] [--compare-userjs <path>]`,
+    );
+    process.exit(1);
+  }
+  ff_diff(version1, version2);
+})();
